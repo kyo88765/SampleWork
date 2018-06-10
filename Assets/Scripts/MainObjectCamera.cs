6e1195b8014c7a5de2d87cfd060219426ad17b56
kyo88765 <kyo88765@gmail.com>
2018-06-10 18:27:50 +0900

    First Commit

---
+++ Assets/Scripts/MainObjectCamera.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class MainObjectCamera : MonoBehaviour
+{
+    [SerializeField]
+    Transform mainObjectTransform;
+
+    void LateUpdate()
+    {
+        UpdatePosition();
+    }
+
+    private void UpdatePosition()
+    {
+        var pos = mainObjectTransform.position + (transform.forward * -10f);
+        transform.position = Vector3.Slerp(transform.position, pos, Time.deltaTime * 100f);
+    }
+
+    public void AddRotation(float x, float y)
+    {
+        // 縦回転
+        var rot = Quaternion.AngleAxis(x, transform.right) * transform.rotation;
+        var euler = rot.eulerAngles;
+        euler.x = euler.x > 180f ? euler.x - 360f : euler.x;
+        euler.x = Mathf.Clamp(euler.x, -45f, 45f);
+        euler.z = 0;
+        rot = Quaternion.Euler(euler);
+        // 横回転
+        transform.rotation = Quaternion.AngleAxis(y, Vector3.up) * rot;
+    }
+}
---
+++ Assets/Scripts/TouchController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TouchController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    [SerializeField]
+    MainObjectCamera mainObjectCamera;
+
+    public void OnPointerDown(PointerEventData data)
+    {
+    }
+
+    public void OnPointerUp(PointerEventData data)
+    {
+    }
+
+    public void OnBeginDrag(PointerEventData data)
+    {
+    }
+
+    public void OnDrag(PointerEventData data)
+    {
+        mainObjectCamera.AddRotation(data.delta.y, data.delta.x);
+    }
+
+    public void OnEndDrag(PointerEventData data)
+    {
+    }
+}
